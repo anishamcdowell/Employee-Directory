485d970b11e6e563efe00c1ebcb646fafc1fb36b
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-31 11:33:23 -0400

    search box UI added

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -6,25 +6,24 @@ function SearchForm(props) {
   return (
     <form className="search">
       <div className="form-group">
-        <label htmlFor="breed">Breed Name:</label>
         <input
           value={props.search}
           onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
+          name="employee"
+          list="employees"
           type="text"
           className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
+          placeholder="Search"
+          id="employee"
         />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
+        {/* <datalist id="employees">
+          {props.employees.map(employee => (
+            <option value={employee} key={employee} />
           ))}
-        </datalist>
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+        </datalist> */}
+        {/* <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
           Search
-        </button>
+        </button> */}
       </div>
     </form>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import SearchForm from "../components/SearchForm";
 
 function Home() {
   return (
@@ -14,7 +15,7 @@ function Home() {
       <Container style={{ marginTop: 30 }}>
         <Row>
           <Col size="md-12">
-            <h1>Welcome To Pupster!</h1>
+            <SearchForm />
           </Col>
         </Row>
         <Row>
