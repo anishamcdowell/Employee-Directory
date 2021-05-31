342fd2788ee55a6595dbae517a37a7b3ada33ff8
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-31 10:46:39 -0400

    Hero/header component finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Employee from "./pages/Employee";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/search" component={Search} /> */}
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
