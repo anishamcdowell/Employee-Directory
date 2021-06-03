3f26cab01e9b1fac0454745f4c0ad841a2701831
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-02 21:49:32 -0400

    Updates

--- src/components/EmployeeRow/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function EmployeeRow(props) {
-  return <div className="row shadow p-3 mb-5 bg-white rounded border-top">{props.children}</div>;
-}
-
-export default EmployeeRow;
